cc034e5bc31638c480dea1fb35c23f16c01bec8d
anapoltronieri <anaclarapoltronieri@hotmail.com>
2016-06-02 17:44:54 -0300

    readme.md
    Criado as colunas da tabela "profissional"

--- database/migrations/2016_06_02_194156_create_profissionals_table.php
+++ database/migrations/2016_06_02_194156_create_profissionals_table.php
@@ -14,7 +14,25 @@ class CreateProfissionalsTable extends Migration
     {
         Schema::create('profissionals', function (Blueprint $table) {
             $table->increments('id');
-            $table->timestamps();
+            $table->varchar(45)('nome');
+			$table->varchar(45)('email');
+			$table->varchar(45)('senha');
+			$table->int('documento');
+			$table->varchar(45)('razaoSocial');
+			$table->varchar(1)('sexo');
+			$table->varchar(45)('endereco');
+			$table->int(10)('telefone');
+			$table->int(11)('celular');
+			$table->varchar(45)('profissao');
+			$table->varchar(45)('registroProfissional');
+			$table->int('numeroRegistro');
+			$table->varchar(2000)('descricao');
+			$table->int('likes');
+			$table->int('deslike');
+			$table->varchar(45)('link1');
+			$table->varchar(45)('link2');
+			$table->varchar(45)('link3');
+			$table->int('visualisacoes');
         });
     }
 
