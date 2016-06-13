795fd43eea914cf0c0366937e50650e5a070a97a
loydecruz <loydecruz@gmail.com>
2016-06-13 18:32:00 -0300

    ProfissionalController

---
+++ app/Http/Controllers/ProfissionalController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace ServJobs\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use ServJobs\Http\Requests;
+
+use Requests;
+
+use ServJobs\Profissional;
+
+class ProfissionalController extends Controller
+{
+    public function busca{
+
+    	$prof = Profissional::all();
+		
+		return view('profissional.telapesquisa')->withProfissional($prof);
+    }
+
+    public function mostra($id){
+		
+		$prof = Profissional::find($id);
+		if(empty($prof)) {
+			return "Usuário não cadastrado";
+		}
+		return view('profissional.resultadopesquisa')->with('p', $produto);
+	}
+
+	public function novo($var){
+		if($var==1){
+			return view('profissional.formulario');
+		} else{
+			
+		}
+		
+	}
+}
