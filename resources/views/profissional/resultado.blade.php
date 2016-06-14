40d4bbc012fe4aa47c11ef054a36011f11bd0c60
loydecruz <loydecruz@gmail.com>
2016-06-14 14:58:21 -0300

    Blade
    Criação das blades principal, pesquisa e resultado.

---
+++ resources/views/profissional/resultado.blade.php
@@ -0,0 +1,18 @@
+@extends('profissional.pesquisa')
+
+@section('resultado')
+<form method ="post">
+<fieldset> <table>
+	<div id="borda">
+	</div>
+	<div id="texto">
+	<p>NOME: Francisco De Oliveira.</p>
+	<p>PROFISSAO: Jardineiro.</p>
+	<p>REGIAO: Vitoria - ES</p>
+</div>
+</fieldset>
+
+</table>
+</form>
+
+@stop
