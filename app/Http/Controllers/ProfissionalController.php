472ecae556b6e92233fe7a396281326dd5a615f9
loydecruz <loydecruz@gmail.com>
2016-06-15 13:44:19 -0300

    ProfissionalController
    Alteração das funções

--- app/Http/Controllers/ProfissionalController.php
+++ app/Http/Controllers/ProfissionalController.php
@@ -20,14 +20,61 @@ class ProfissionalController extends Controller
 		return view('profissional.listagem')->withProfissional($prof);
 	}
 
-    public function busca{
+    public function busca(){
 
     	$prof = Profissional::all();
 		
 		return view('profissional.telapesquisa')->withProfissional($prof);
     }
 
-    public function mostra($id){
+    public function visualizar($id){
+		
+		$prof = Profissional::find($id);
+		if(empty($prof)) {
+			return "Usuário não cadastrado";
+		}
+		return view('profissional.resultadopesquisa')->with('p', $produto);
+	}
+
+	public function novo($var){
+		if($var==1){
+			return view('profissional.formulario');
+		} else{
+
+		}
+		
+	}
+}
+<?php
+
+namespace ServJobs\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use ServJobs\Http\Requests;
+
+use Requests;
+
+use ServJobs\Profissional;
+
+class ProfissionalController extends Controller
+{
+    
+    public function lista(){
+		
+		$prof = Profissional::all();
+		
+		return view('profissional.listagem')->withProfissional($prof);
+	}
+
+    public function busca(){
+
+    	$prof = Profissional::all();
+		
+		return view('profissional.telapesquisa')->withProfissional($prof);
+    }
+
+    public function visualizar($id){
 		
 		$prof = Profissional::find($id);
 		if(empty($prof)) {
