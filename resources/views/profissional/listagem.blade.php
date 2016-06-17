ebbb74e634eff1d51a87e2e9acb82d7bfde6cc5c
loydecruz <loydecruz@gmail.com>
2016-06-17 16:19:09 -0300

    listagem.blade
    Alteração da variável de parâmetro da view

--- resources/views/profissional/listagem.blade.php
+++ resources/views/profissional/listagem.blade.php
@@ -1,18 +1,20 @@
 @extends('profissional.pesquisa')
 
 @section('resultado')
-<form method ="post">
-<fieldset> <table>
-	<div id="borda">
-	</div>
-	<div id="texto">
-	<p>NOME: Francisco De Oliveira.</p>
-	<p>PROFISSAO: Jardineiro.</p>
-	<p>REGIAO: Vitoria - ES</p>
-</div>
-</fieldset>
 
-</table>
+<h1>Listagem de profissional</h1>
+	<table>
+	<?php foreach ($profissional as $p): ?>
+			<tr>
+				<td><?= $p->nome ?> </td>
+				<td><?= $p->cargo ?> </td>
+				<td><?= $p->cidade ?> </td>
+				<td><?= $p->uf ?> </td>
+				
+			</tr>
+		<?php endforeach ?>
+	</table>
+
 </form>
 
 @stop
