d5c840f981607f817dbcc54988c820fc32a54734
loydecruz <loydecruz@gmail.com>
2016-06-21 14:23:39 -0300

    Update principal.blade.php
    Alteração da tela principal

--- resources/views/layout/principal.blade.php
+++ resources/views/layout/principal.blade.php
@@ -1,58 +1,92 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
     <head>
         <title>ServJobs</title>
         <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
-        <link href="css/estilo.css" rel="stylesheet" />
-        <!--<link href="css/fontes.css" rel="stylesheet" />-->
+
+        <!--/////IMPORTAÇÕES/////-->
+        <link href="css/principal.css" rel="stylesheet" />
+        <link href="css/font-awesome.css" rel="stylesheet" />
+        <link href="css/font-awesome.min.css" rel="stylesheet" />
+        <link href="/css/app.css" rel="stylesheet">
+        <link href="/css/estiloListagem.css" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="/css/bootstrap.css">
+        <link href="css/fontes.css" rel="stylesheet" />
+        <link rel="stylesheet" href="css/normalizandoLogin.css">
+        <link rel="stylesheet" href="css/loginCadastroContato.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+        <script src="js/bootstrap.js"></script>
+        <script src="js/bootstrap.min.js"></script>
         <script src="js/main.js"></script>
+        <script src="js/index.js"></script>
+        <!--/////IMPORTAÇÕES ACABA AQUI/////-->
 
     </head>
-    <body>
-        <div>
-            <header>
-                <!-- menu principal -->
-                <div class="menu_bar">
-                    <a href="#" class="btn-menu1">
-                        <span class="icon-menu-sobe"></span>
-                        Menu
-                    </a>
-                </div>
-                <div class="menu_bar">
-                    <a href="#" class="btn-menu2">
-                        <span class="icon-menu-desce"></span>
-                        Menu
-                    </a> 
-                </div>
+ 
+<body>
 
-                
-                <!-- navegador do menu principal -->
-                <nav>
-                    <span id="logotipo">Imagem do Logotipo</span>
-                    <!-- lista de icones para a resolução acima de 800px, no arquivo de css foi utilizado
-                         float right para alinhar os icones ao lado, e o outro menu perde a caracteristica
-                         float quando em dispositivos mobiles -->
-                    <ul>
-                        <li><a href="#"><span class="icon-email"></span>Cadastre-se</a></li>
-                        <li><a href="#"><span class="icon-html5"></span>Produtos</a></li>
-                        <li><a href="#"><span class="icon-olho"></span>Empresa</a></li>
-                        <li><a href="#"><span class="icon-home"></span>Home</a></li>     
-                    </ul>
-                    
-                    <!-- lista de icones para a resolução mobile -->
-                    <ul id="menuMobile">
-                        <li><a href="#"><span class="icon-home"></span>Home</a></li>
-                        <li><a href="#"><span class="icon-olho"></span>Empresa</a></li>
-                        <li><a href="#"><span class="icon-html5"></span>Produtos</a></li>
-                        <li><a href="#"><span class="icon-email"></span>Cadastre-se</a></li>
-                    </ul>
-                </nav>
-            </header>
-        </div>
-        <div>
-            @yield('conteudo')
-        </div>
-    </body>
+      <!--/////////////CABEÇALHO/////////////-->
+      <div>
+      <header>
+      <nav>
+           <!--******ESPAÇO PARA A LOGOTIPO******-->
+            <span id="logotipo">
+                    <img src="img/servlogo.png" width="100px" height="108px">
+            </span>
+           <!--******ESPAÇO PARA A LOGOTIPO ACABA AQUI*****-->
+
+      <ul>
+                              
+           <li><a href="login.html"><span class="#"></span><br>LOGIN</a></li>
+           <li><a href="contato.html"><span class="#"></span><br>CONTATO</a></li>
+           <li><a href="#Pesquisa"><span class="#"></span><br>PESQUISA</a></li>
+           <li><a href="#"><span class="#"></span><br>COMO FUNCIONA</a></li>
+           <li><a href="#"><span class="#"></span><br>HOME</a></li>     
+      </ul>
+      <a name = "topo"> </a>
+      </nav>
+      </header>
+      </div>
+      <!--/////////////CABEÇALHO ACABA AQUI/////////////-->
+
+
+
+
+      <!--/////////////SEÇÃO DE IMAGEM PARA SLIDE/////////////-->
+      <div id="slide">
+      <section>
+            
+            <img src="img/ServBanner4.jpg">
+            <div class="msgBanner">
+            <h1>ServJobs</h1>
+            <p><h2>Seja bem-vindo</h2></p>
+
+      </section>
+      </div>
+      <!--/////////////AQUI FINALIZA A SEÇÃO DE IMAGEM PARA SLIDE/////////////-->
+
+      <div class="container">
+      @yield('conteudo')
+      </div>
+
+      
+      <!--/////////////RODAPÉ/////////////-->
+      <div>
+      <footer id="rodape">
+
+            <p><br><br><br>Copyrigth &copy; 2016 - ServJobs
+            <a href="#" target="_blank">&nbsp Facebook</a>
+            <a href="#" target="_blank">&nbsp Twitter</a>  
+
+      </footer>
+      </div>
+      <!--/////////////RODAPÉ ACABA AQUI/////////////-->
+
+
+
+
+</body>
+
 </html>
